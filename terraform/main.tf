1d7eb7f00aa416b13decb1f42a9900afc1dde1c3
Visay Keo <keo@visay.info>
2022-08-26 18:27:11 +0700

    [TASK] Add initial terraform config

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  cloud {
+    organization = "prettygreenplants"
+
+    workspaces {
+      name = "prettygreenplants_com"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "registry.terraform.io/hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c19b15b5c2b52789"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
