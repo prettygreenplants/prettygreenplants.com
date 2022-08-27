e1f3d43e6dcf95a3c33878688200139866597bfb
Visay Keo <keo@visay.info>
2022-08-27 12:22:22 +0700

    [TASK] More improvement

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -1,4 +1,4 @@
-
+# System
 provider "aws" {
   region = var.aws_region
 
@@ -9,48 +9,17 @@ provider "aws" {
   }
 }
 
+# Constants
 resource "aws_iam_account_alias" "alias" {
   account_alias = "pgp2022"
 }
 
-resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.app_server.id
-  allocation_id = aws_eip.pgp2022.id
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-02ee763250491e04a"
-  instance_type = "t2.micro"
-  key_name= "keypair-pgp2022"
-  vpc_security_group_ids = [aws_security_group.main.id]
-
-  connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ubuntu"
-      private_key = var.private_key
-      timeout     = "4m"
-   }
-
-  tags = {
-    Name = "ec2-pgp2022"
-  }
-}
-
-resource "aws_eip" "pgp2022" {
-  vpc = true
-
-  tags = {
-    Name = "eip-pgp2022"
-  }
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "keypair-pgp2022"
+resource "aws_key_pair" "keypair" {
+  key_name   = "keypair-prettygreenplants"
   public_key = var.public_key
 }
 
-resource "aws_security_group" "main" {
+resource "aws_security_group" "sg" {
   egress = [
     {
       cidr_blocks      = [ "0.0.0.0/0", ]
@@ -64,17 +33,78 @@ resource "aws_security_group" "main" {
       to_port          = 0
     }
   ]
- ingress                = [
-   {
-     cidr_blocks      = [ "0.0.0.0/0", ]
-     description      = ""
-     from_port        = 22
-     ipv6_cidr_blocks = []
-     prefix_list_ids  = []
-     protocol         = "tcp"
-     security_groups  = []
-     self             = false
-     to_port          = 22
-  }
+  ingress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    }
   ]
+
+  tags = {
+    Name = "sg-inbound"
+  }
+}
+
+resource "aws_instance" "hosting-server" {
+  ami                    = "ami-055147723b7bca09a"
+  instance_type          = "t2.micro"
+  key_name               = "keypair-prettygreenplants"
+  vpc_security_group_ids = [
+    aws_security_group.sg.id
+  ]
+
+  root_block_device {
+    volume_size = 30
+  }
+
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = var.private_key
+      timeout     = "4m"
+   }
+
+  tags = {
+    Name = "hosting-server"
+  }
+}
+
+
+resource "aws_eip" "elastic-ip" {
+  vpc = true
+
+  tags = {
+    Name = "public-ip"
+  }
+}
+
+resource "aws_eip_association" "elastic-ip-association" {
+  instance_id   = aws_instance.hosting-server.id
+  allocation_id = aws_eip.elastic-ip.id
+}
+
+resource "aws_s3_bucket" "bucket-backup" {
+  bucket = "pgp2022-website-backup"
+
+  tags = {
+    Name        = "backup"
+    Environment = var.context
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket-backup-access" {
+  bucket = aws_s3_bucket.bucket-backup.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
--- terraform/production/variables.tf
+++ terraform/production/variables.tf
@@ -10,10 +10,10 @@ variable "context" {
 
 variable "private_key" {
   type        = string
-  default     = "file(~/.ssh/pgp2022.pem)"
+  default     = "file(~/.ssh/prettygreenplants.pem)"
 }
 
 variable "public_key" {
   type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCk/nazrwyl2ZKRBQV4QopcR4x/kXnCJx52Uu1e814Qpcz/jYFnU4fNM5dcDh0e07MdCCmuG2LagmB7z8Bl1xRhV5+CexQuYp/cjSmbNVngVshxRRyJ/2KiI73Xl0mGOMaGLkwCDsNwW6tcumRlJMG5yWvGfSQVPSlsnlhX4KiISiAo09MBqlnGzQU73zsUxmv0+zBECVp7gsF30Pi9vrm9gLaGTs1vipc1sdynHcJb5rZqDjoZP3mZW8/OAe7uU8HDkHnLhZNypTZXUhtIkYmXITDjbGC/QYF1vUW6Qn/yJLDLU99FNQzzoS9dWmRsS8Hp19Tc7zYUEC7FkoKGQAsb pgp2022"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCk/nazrwyl2ZKRBQV4QopcR4x/kXnCJx52Uu1e814Qpcz/jYFnU4fNM5dcDh0e07MdCCmuG2LagmB7z8Bl1xRhV5+CexQuYp/cjSmbNVngVshxRRyJ/2KiI73Xl0mGOMaGLkwCDsNwW6tcumRlJMG5yWvGfSQVPSlsnlhX4KiISiAo09MBqlnGzQU73zsUxmv0+zBECVp7gsF30Pi9vrm9gLaGTs1vipc1sdynHcJb5rZqDjoZP3mZW8/OAe7uU8HDkHnLhZNypTZXUhtIkYmXITDjbGC/QYF1vUW6Qn/yJLDLU99FNQzzoS9dWmRsS8Hp19Tc7zYUEC7FkoKGQAsb info@prettygreenplants.com"
 }
