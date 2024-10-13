138ae2f1dee4cdaf981c37fc5a94857ce9777527
Visay Keo <visay@web-essentials.co>
2024-10-13 18:11:02 +0700

    [TASK] Upgrade to Neos 4.1

--- scripts/syncontent.sh
+++ scripts/syncontent.sh
@@ -28,6 +28,7 @@ if [ ${ERROR} == "0" ]; then
 
 	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 		LOCAL_BASE_PATH=`pwd`
-		ansible-playbook ansible/playbook.yml --limit ${STAGE} --tags syncontent --extra-vars "base_dir=${LOCAL_BASE_PATH} from_date=${FROM_DATE} trusted_proxies='*'"
+		source ${LOCAL_BASE_PATH}/.env
+		ansible-playbook ansible/playbook.yml --limit ${STAGE} --tags syncontent --extra-vars "base_dir=${LOCAL_BASE_PATH} from_date=${FROM_DATE} trusted_proxies='*' project_context=${FLOW_CONTEXT}"
 	fi
 fi
